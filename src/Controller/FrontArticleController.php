961f657ff6f8dafecc6be9026d0c93ef8494dbeb
jerome <ambroise.jerome.formation@gmail.com>
2019-04-05 14:31:37 +0200

    Liste des articles - Affichage avec liste a puces

--- src/Controller/FrontArticleController.php
+++ src/Controller/FrontArticleController.php
@@ -1,6 +1,7 @@
 <?php
 namespace App\Controller;
 
+use App\Entity\Article;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -8,12 +9,23 @@ use Symfony\Component\Routing\Annotation\Route;
 class FrontArticleController extends AbstractController
 {
     /**
+     * Liste les derniers articles
      * @Route("/")
      * @return Response
      */
     public function list(): Response
     {
-        return $this->render('index.html.twig');
+        // Récupération du Repository
+        $repository = $this->getDoctrine()
+            ->getRepository(Article::class);
+
+        // Récupération de tous les articles
+        $articles = $repository->findAll();
+
+        // Renvoi des articles à la vue
+        return $this->render('index.html.twig', [
+            'articles' => $articles
+        ]);
     }
 
     /**
